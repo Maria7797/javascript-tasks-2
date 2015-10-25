37a30ce4b15a6add81dcb4d93dc7aa1e3fbfbd39
Maria7797 <bmw_7797@mail.ru>
2015-10-25 21:52:46 +0500

    Update phoneBook.js
    @sameoldmadness, посмотрите исправления пожалуйста

--- phoneBook.js
+++ phoneBook.js
@@ -1,3 +1,6 @@
+'use strict'
+
+
 var phoneBook = []; // Здесь вы храните записи как хотите
 
 /*
@@ -5,50 +8,52 @@ var phoneBook = []; // Здесь вы храните записи как хот
    На вход может прийти что угодно, будьте осторожны.
 */
 module.exports.add = function add(name, phone, email) {
-  if(ValidPhone(phone) && ValidEmail(email)) {
+  if(isValidPhone(phone) && isValidEmail(email)) {
       var person = {
-    name: name, 
-    phone: phone, 
-    email: email
-  };
-  phoneBook.push(person);
+          name: name, 
+          phone: phone, 
+          email: email
+          
+      };
+      phoneBook.push(person);
   }
 };
 
-function ValidEmail(email) {
-  var Ve = /^\w+[@]+[a-z]+\.+[a-z]+$/;
-  return Ve.test(email);
-};
+function isValidPhone(phone) {
+    return /^((\+?\d{1,4})[\- ]?)?((\(\d{3}\)|\d{3})[\- ]?)[\d\- ]{7,10}$/.test(phone); 
+} 
 
-function ValidPhone(phone) {
-  var Vp = /^\+?\d+$/g;
-  return Vp.test(phone);
-};
+function isValidEmail(email) {
+    return /^[\w\d\.-]+@([\w\u0410-\u044F\d-]+)(\.[\w\u0410-\u044F\d]+)+$/.test(email); 
+}
 
 /*
    Функция поиска записи в телефонную книгу.
    Поиск ведется по всем полям.
 */
 module.exports.find = function find(query) {
-  for(var i = 0; i < phoneBook.length; i++) {
-    if (!phoneBook[i].name.indexOf(query) || !phoneBook[i].phone.indexOf(query) || !phoneBook[i].email.indexOf(query)) {
-      console.log(phoneBook[i].name + ', ' + phoneBook[i].phone + ', ' + phoneBook[i].email);
-    }            
-  }
+    if(!query) {
+        console.log(phoneBook);
+    }
+    
+    for(var i = 0; i < phoneBook.length; i++) {
+        if (!phoneBook[i].name.indexOf(query) || !phoneBook[i].phone.indexOf(query) || !phoneBook[i].email.indexOf(query)) {
+            console.log(phoneBook[i].name + ', ' + phoneBook[i].phone + ', ' + phoneBook[i].email);
+        }
+    }
 };
 
 /*
    Функция удаления записи в телефонной книге.
 */
 module.exports.remove = function remove(query) {
-  var sum=0;
-
-  for(var i = 0; i < phoneBook.length; i++) {
-    if (!phoneBook[i].name.indexOf(query) || !phoneBook[i].phone.indexOf(query) || !phoneBook[i].email.indexOf(query)) {
-      phoneBook.splice(i, 1);
-      sum++;
+    var sum=0;
+    
+    for(var i = 0; i < phoneBook.length; i++) {
+        if (!phoneBook[i].name.indexOf(query) || !phoneBook[i].phone.indexOf(query) || !phoneBook[i].email.indexOf(query)) {
+            phoneBook.splice(i, 1);
+            sum++;
+        }
     }
-  }
-
-  console.log('Удалено: ', sum);
+    console.log('Удалено: ', sum);
 };
